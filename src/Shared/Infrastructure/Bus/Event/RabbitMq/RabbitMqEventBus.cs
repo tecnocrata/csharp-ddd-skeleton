0f443e1101edb1fcb668278639a88ee215532de2
Enrique Ortuno <enrique@ortuno.net>
2024-05-26 22:40:27 -0400

    initial upgrade changes

--- src/Shared/Infrastructure/Bus/Event/RabbitMq/RabbitMqEventBus.cs
+++ src/Shared/Infrastructure/Bus/Event/RabbitMq/RabbitMqEventBus.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using CodelyTv.Shared.Domain.Bus.Event;
 using CodelyTv.Shared.Infrastructure.Bus.Event.MsSql;
@@ -22,7 +23,7 @@ namespace CodelyTv.Shared.Infrastructure.Bus.Event.RabbitMq
 
         public async Task Publish(List<DomainEvent> events)
         {
-            events.ForEach(async e => await Publish(e));
+            await Task.WhenAll(events.Select(e => Publish(e)));
         }
 
         private async Task Publish(DomainEvent domainEvent)
@@ -32,9 +33,9 @@ namespace CodelyTv.Shared.Infrastructure.Bus.Event.RabbitMq
                 var serializedDomainEvent = DomainEventJsonSerializer.Serialize(domainEvent);
                 _rabbitMqPublisher.Publish(_exchangeName, domainEvent.EventName(), serializedDomainEvent);
             }
-            catch (RabbitMQClientException e)
+            catch (RabbitMQClientException)
             {
-                await _failOverPublisher.Publish(new List<DomainEvent> {domainEvent});
+                await _failOverPublisher.Publish(new List<DomainEvent> { domainEvent });
             }
         }
     }
